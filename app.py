b05694b94f657e57b31366bd24012300250158bf
Peri Ridwan Nurhedi <periridwan35@gmail.com>
2022-01-09 22:47:46 +0700

    menambahkan resource

---
+++ app.py
@@ -0,0 +1,36 @@
+from flask import Flask, request
+from flask_restful import Resource, Api
+from flask_cors import CORS
+from werkzeug.wrappers import response
+
+# inisiasi object
+app = Flask(__name__)
+
+# inisiasi object flask restful
+api = Api(app)
+
+# inisiasi object cors
+CORS(app)
+
+# inisiasi variable kosong bertipe dictionary
+identitas = {} #variable global, dictionary = json
+
+# membuat class resource
+class ContohResource(Resource):
+    # buat method get dan post
+    def get(self):
+        # response = {"msg": "Hello World! It's my first Restful"}
+        return identitas
+    
+    def post(self):
+        nama = request.form["nama"]
+        kelas = request.form["kelas"]
+        identitas["nama"] = nama
+        identitas["umur"] = kelas
+        response = {"msg": "Data berhasil dimasukkan"}
+        return response
+
+api.add_resource(ContohResource, "/api", methods=["GET", "POST"])
+
+if __name__ == "__main__":
+    app.run(debug=True, port=5005)
